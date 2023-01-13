67a4491083f52765ab3094b5f6e5165722eeb5cc
trantuvan <trantuvan.kan@gmail.com>
2023-01-13 15:19:51 +0700

    add new grid template cols

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "1-4": "1fr 4fr",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
